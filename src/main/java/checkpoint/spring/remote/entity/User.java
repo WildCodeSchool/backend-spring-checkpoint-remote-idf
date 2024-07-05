8a2b2ec3f97e575cb9c8523de1cc68932c2bb59b
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-07-05 09:37:03 +0200

    add User entity

---
+++ src/main/java/checkpoint/spring/remote/entity/User.java
@@ -0,0 +1,49 @@
+package checkpoint.spring.remote.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstname;
+
+    private String lastname;
+
+    public User() {
+    }
+
+    public User(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+}
