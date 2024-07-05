1ef681cd654efa5711bd6af6b4e92aa248cad647
Vivi Valienne <valiendanse@gmail.com>
2024-07-05 12:28:25 +0200

    last

--- src/main/java/checkpoint/spring/remote/RemoteApplication.java
+++ src/main/java/checkpoint/spring/remote/RemoteApplication.java
@@ -1,7 +1,12 @@
 package checkpoint.spring.remote;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import checkpoint.spring.remote.entity.User;
+import checkpoint.spring.remote.repository.UserRepository;
 
 @SpringBootApplication
 public class RemoteApplication {
@@ -10,11 +15,11 @@ public class RemoteApplication {
 		SpringApplication.run(RemoteApplication.class, args);
 	}
 
-	// @Bean
-	// public CommandLineRunner demo(UserRepository repository) {
-	// return (args) -> {
-	// repository.save(new User("Joe", "Start"));
-	// repository.save(new User("Baden", "Powell"));
-	// };
-	// }
+	@Bean
+	public CommandLineRunner demo(UserRepository repository) {
+		return (args) -> {
+			repository.save(new User("Joe", "Start"));
+			repository.save(new User("Baden", "Powell"));
+		};
+	}
 }
