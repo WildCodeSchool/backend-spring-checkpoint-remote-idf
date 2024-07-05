433ff6e1d79f3c818e5549639e80cbf3f344a24b
LuDanis <lucie.lindanis@gmail.com>
2024-07-05 10:03:23 +0200

    STEP 1

---
+++ src/main/java/checkpoint/spring/remote/controller/UserController.java
@@ -0,0 +1,23 @@
+package checkpoint.spring.remote.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import checkpoint.spring.remote.repository.UserRepository;
+import checkpoint.spring.remote.entity.User;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+    
+    @GetMapping("")
+    public List<User> getAll() {
+        return this.userRepository.findAll();
+    }
+}
---
+++ src/main/java/checkpoint/spring/remote/repository/UserGeneratorRepository.java
@@ -0,0 +1,29 @@
+package checkpoint.spring.remote.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import checkpoint.spring.remote.entity.User;
+
+@Component
+public class UserGeneratorRepository implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if(this.userRepository.count() == 0){
+            List<User> users = new ArrayList<>();
+
+            users.add(new User("Joe", "Start"));
+            users.add(new User("Baden", "Powell"));
+
+            this.userRepository.saveAll(users);
+        }
+    }
+    
+}
