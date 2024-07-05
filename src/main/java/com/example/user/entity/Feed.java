6675abe6670007a557b84ae408bf13f33a3c404a
marwasliti <marwasliti11888@yahoo.com>
2024-07-05 11:20:00 +0200

    add oneToMany relationship user and feed

---
+++ src/main/java/com/example/user/entity/Feed.java
@@ -0,0 +1,50 @@
+package com.example.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="feed_table")
+public class Feed {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String message;
+    private String imageUrl;
+    public Feed() {
+    }
+
+    public Feed(Long id, String message, String imageUrl) {
+        this.id = id;
+        this.message = message;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getImageUrl() {
+        return this.imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+    
+}
--- src/main/java/com/example/user/entity/User.java
+++ src/main/java/com/example/user/entity/User.java
@@ -1,10 +1,18 @@
 package com.example.user.entity;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Size;
-
+@Entity
+@Table(name="users_table")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,6 +20,9 @@ public class User {
     @Size(min = 2, max = 100, message = "firstname and lastname name has to be between 2 and 10 characters")
     private String firstname;
     private String lastname;
+     @JsonIgnore
+    @OneToMany(mappedBy = "user")
+    private List<Feed> feeds;
     public User(Long id, String firstname, String lastname) {
         this.id = id;
         this.firstname = firstname;
