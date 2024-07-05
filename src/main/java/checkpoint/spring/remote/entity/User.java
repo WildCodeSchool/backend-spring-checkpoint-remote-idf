5e3f55150fc4df60fdb1d689f9106e7d7d463e49
Vivi Valienne <valiendanse@gmail.com>
2024-07-05 12:00:00 +0200

    change name of database in applications-dev.properties

--- src/main/java/checkpoint/spring/remote/entity/User.java
+++ src/main/java/checkpoint/spring/remote/entity/User.java
@@ -1,16 +1,21 @@
 package checkpoint.spring.remote.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "user")
 public class User {
-    
+
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String firstname;
     private String lastname;
 
@@ -42,5 +47,5 @@ public class User {
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
-    
+
 }
