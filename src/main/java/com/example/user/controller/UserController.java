e352b46b397999256e42a16760e605075fe71a34
marwasliti <marwasliti11888@yahoo.com>
2024-07-05 10:40:00 +0200

    creation of root to add user

--- src/main/java/com/example/user/controller/UserController.java
+++ src/main/java/com/example/user/controller/UserController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,12 @@ public class UserController {
        
         List<User> Users = this.userRepo.findAllByName();  
         return Users;
+    }
+      @PostMapping("")
+    public User addOne(@RequestBody User user){
+        user.getFirstname();
+        user.getLastname();
+        return this.userRepo.save(user);
     }
 
 }
