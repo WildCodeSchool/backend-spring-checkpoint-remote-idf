4ee23e7f4dcd0a1c3cdcf3a0d42ecda2284c1b03
Kileana27 <blond.camillelm@gmail.com>
2024-07-05 12:23:19 +0200

    Added relationship

--- src/main/java/checkpoint/spring/remote/entity/Comment.java
+++ src/main/java/checkpoint/spring/remote/entity/Comment.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Comment {
@@ -13,12 +15,27 @@ public class Comment {
     private Long id;
     private String message;
 
+    @ManyToOne
+    @JoinColumn(name="post_id")
+    private Post post;
+
     public Comment() {
     }
 
     public Comment(Long id, String message) {
         this.id = id;
         this.message = message;
+    }
+
+    public Comment(Long id, String message, Post post) {
+        this.id = id;
+        this.message = message;
+        this.post = post;
+    }
+
+    public Comment(String message, Post post) {
+        this.message = message;
+        this.post = post;
     }
 
     public Long getId() {
@@ -31,5 +48,13 @@ public class Comment {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
     }
 }
--- src/main/java/checkpoint/spring/remote/entity/Post.java
+++ src/main/java/checkpoint/spring/remote/entity/Post.java
@@ -1,9 +1,14 @@
 package checkpoint.spring.remote.entity;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Post {
@@ -14,10 +19,15 @@ public class Post {
     private String message;
     private String imageUrl;
 
+    @JsonIgnore
+    @OneToMany(mappedBy= "post")
+    private List<Comment> comments;
+
     public Post() {
     }
 
-    public Post(Long id, String imageUrl, String message) {
+    public Post(List<Comment> comments, Long id, String imageUrl, String message) {
+        this.comments = comments;
         this.id = id;
         this.imageUrl = imageUrl;
         this.message = message;
@@ -43,6 +53,13 @@ public class Post {
         this.imageUrl = imageUrl;
     }
 
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
 
     
 }
