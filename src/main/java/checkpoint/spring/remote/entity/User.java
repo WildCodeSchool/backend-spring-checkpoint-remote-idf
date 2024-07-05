8fda18387d8720f3ab419776bb541c0ecfcb6b3d
clotildebeckerlamoureux <clobecker77@gmail.com>
2024-07-05 09:49:52 +0200

    checkpoint config and user entity created

---
+++ src/main/java/checkpoint/spring/remote/entity/User.java
@@ -0,0 +1,49 @@
+package checkpoint.spring.remote.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstname;
+
+    private String lastname;
+
+    public User() {}
+
+    public User(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getFirstname() {
+        return this.firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return this.lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+}
