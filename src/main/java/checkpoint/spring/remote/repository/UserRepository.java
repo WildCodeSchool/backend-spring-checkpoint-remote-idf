566a792fa599ed6d43bc1ecb5caaca3c242dfa78
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-07-05 10:01:26 +0200

    Add annotation

--- src/main/java/checkpoint/spring/remote/repository/UserRepository.java
+++ src/main/java/checkpoint/spring/remote/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package checkpoint.spring.remote.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import checkpoint.spring.remote.entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
--- src/main/java/checkpoint/spring/remote/service/UserService.java
+++ src/main/java/checkpoint/spring/remote/service/UserService.java
@@ -3,10 +3,12 @@ package checkpoint.spring.remote.service;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import checkpoint.spring.remote.entity.User;
 import checkpoint.spring.remote.repository.UserRepository;
 
+@Service
 public class UserService {
 
     @Autowired
