83def75737bb767a71d2383442d5c9a32273e02f
marwasliti <marwasliti11888@yahoo.com>
2024-07-05 10:16:02 +0200

    creation users route and display user list

--- src/main/java/com/example/user/controller/UserController.java
+++ src/main/java/com/example/user/controller/UserController.java
@@ -1,10 +1,26 @@
 package com.example.user.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.user.entity.User;
+import com.example.user.repository.UserRepository;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    
+    @Autowired
+    UserRepository userRepo;
+
+     @GetMapping("")
+    public List<User> getAll() {
+       
+        List<User> Users = this.userRepo.findAllByName();  
+        return Users;
+    }
+
 }
