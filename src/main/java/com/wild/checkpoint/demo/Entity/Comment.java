cfac8e0d9a182de2628233a6ce484e4e10af13bd
Cyril <c.concas.ext@axione.fr>
2024-07-05 12:36:28 +0200

    Final

--- src/main/java/com/wild/checkpoint/demo/Entity/Comment.java
+++ src/main/java/com/wild/checkpoint/demo/Entity/Comment.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Comment {
@@ -18,6 +19,7 @@ public class Comment {
 
     public Comment(){}
 
+    @ManyToOne()
     @JoinColumn(name = "post_id")
     private Posts posts;
 
